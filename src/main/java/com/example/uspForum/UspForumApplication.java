52bb7b5e7ae248e9d1459ab70cf40966db0db8c5
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-28 13:02:24 -0300

    Initialize Spring
    Initialized with Spring Web, Thymeleaf and PostgreSQL Driver

---
+++ src/main/java/com/example/uspForum/UspForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.uspForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UspForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UspForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uspForum/UspForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uspForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UspForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

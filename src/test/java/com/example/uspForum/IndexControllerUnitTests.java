68635a200eba1a17a83a4949a532561092135c11
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-29 10:48:06 -0300

    Implement getIndexReturnsCorrectHtml test

--- src/test/java/com/example/uspForum/IndexControllerUnitTests.java
+++ src/test/java/com/example/uspForum/IndexControllerUnitTests.java
@@ -1,4 +1,17 @@
 package com.example.uspForum;
 
+import com.example.uspForum.controller.IndexController;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IndexControllerUnitTests {
+
+    @Test
+    public void getIndexReturnsCorrectHtml() {
+        IndexController indexController = new IndexController();
+        String returnedHtml = indexController.index();
+        assertEquals("index.html", returnedHtml);
+    }
+
 }

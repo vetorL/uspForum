945dea8e7f4664d4586cf93cb3659dabdbebc969
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-29 11:12:09 -0300

    Implement testPostSearchReturnsCorrectHtml test

--- src/test/java/com/example/uspForum/SearchControllerUnitTests.java
+++ src/test/java/com/example/uspForum/SearchControllerUnitTests.java
@@ -1,4 +1,28 @@
 package com.example.uspForum;
 
+import com.example.uspForum.controller.SearchController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class SearchControllerUnitTests {
+
+    @Mock
+    private Model model;
+
+    @InjectMocks
+    private SearchController searchController;
+
+    @Test
+    public void testPostSearchReturnsCorrectHtml() {
+        String result = searchController.search("", model);
+        assertEquals("busca.html", result);
+    }
+
 }

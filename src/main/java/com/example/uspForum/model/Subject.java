e3202b732f9e8bf80e95687518936f1b48c3b026
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 14:07:32 -0300

    Generate getters and setters for Subject

--- src/main/java/com/example/uspForum/model/Subject.java
+++ src/main/java/com/example/uspForum/model/Subject.java
@@ -14,4 +14,59 @@ public class Subject {
     private long teacherId;
     private long createdByAccountId;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getRelatedCourse() {
+        return relatedCourse;
+    }
+
+    public void setRelatedCourse(String relatedCourse) {
+        this.relatedCourse = relatedCourse;
+    }
+
+    public long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(long teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public long getCreatedByAccountId() {
+        return createdByAccountId;
+    }
+
+    public void setCreatedByAccountId(long createdByAccountId) {
+        this.createdByAccountId = createdByAccountId;
+    }
 }

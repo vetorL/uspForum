aa3edab90d3c19f08d277cc0a846cdfeda25c467
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 11:44:36 -0300

    Add attribute results to model

--- src/main/java/com/example/uspForum/controller/SearchController.java
+++ src/main/java/com/example/uspForum/controller/SearchController.java
@@ -1,10 +1,14 @@
 package com.example.uspForum.controller;
 
+import com.example.uspForum.model.Subject;
 import com.example.uspForum.service.SearchService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class SearchController {
@@ -21,7 +25,10 @@ public class SearchController {
             @RequestParam String t,
             Model model) {
 
+        List<Subject> results = new ArrayList<>();
+
         model.addAttribute("q", q);
+        model.addAttribute("results", results);
 
         return "busca.html";
     }

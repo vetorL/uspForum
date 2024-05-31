1325a0ca7feea7d7d8bbe9ffd9d3511161d754f0
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 00:20:49 -0300

    Inject TeacherRepository

--- src/main/java/com/example/uspForum/service/SearchService.java
+++ src/main/java/com/example/uspForum/service/SearchService.java
@@ -1,4 +1,15 @@
 package com.example.uspForum.service;
 
+import com.example.uspForum.repository.TeacherRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SearchService {
+
+    private final TeacherRepository teacherRepository;
+
+    public SearchService(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+
 }

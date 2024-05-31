3da4536c687d93c1c27bc96cfe082684302f1443
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 16:26:02 -0300

    Add findByTeacherId

--- src/main/java/com/example/uspForum/repository/TeacherRepository.java
+++ src/main/java/com/example/uspForum/repository/TeacherRepository.java
@@ -4,11 +4,9 @@ import com.example.uspForum.model.Teacher;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface TeacherRepository extends CrudRepository<Teacher, Long> {
 
-    @Query("SELECT * FROM teacher WHERE teacherName = :teacherName")
-    List<Teacher> findByTeacherName(String teacherName);
+    @Query("SELECT * FROM teacher WHERE id = :id")
+    Teacher findByTeacherId(long id);
 
 }

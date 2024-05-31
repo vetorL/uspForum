ccd2c45e1a811d2024d0c62058fad30f0f8cd209
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 10:54:23 -0300

    Implement Subject model

--- src/main/java/com/example/uspForum/model/Subject.java
+++ src/main/java/com/example/uspForum/model/Subject.java
@@ -1,4 +1,17 @@
 package com.example.uspForum.model;
 
+import org.springframework.data.annotation.Id;
+
 public class Subject {
+
+    @Id
+    private long id;
+
+    private String name;
+    private String abbreviation;
+    private String code;
+    private String relatedCourse;
+    private long teacherId;
+    private long createdByUserId;
+
 }

0854d1e559647691025b1a6d3e6674d9285deb26
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 16:22:08 -0300

    Implement SubjectResult model

--- src/main/java/com/example/uspForum/model/SubjectResult.java
+++ src/main/java/com/example/uspForum/model/SubjectResult.java
@@ -1,4 +1,24 @@
 package com.example.uspForum.model;
 
-public class SubjectResult {
+public class SubjectResult extends Subject {
+    private String teacherName;
+
+    public SubjectResult(Subject subject, String teacherName) {
+        setId(subject.getId());
+        setName(subject.getName());
+        setTeacherName(teacherName);
+        setCode(subject.getCode());
+        setAbbreviation(subject.getAbbreviation());
+        setRelatedCourse(subject.getRelatedCourse());
+        setTeacherId(subject.getTeacherId());
+        setCreatedByAccountId(subject.getCreatedByAccountId());
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
 }

b67fdd2dc1040ced1a3ff54f552a36680084641e
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 11:53:51 -0300

    Implement findSubjectByAbbreviation

--- src/main/java/com/example/uspForum/repository/SubjectRepository.java
+++ src/main/java/com/example/uspForum/repository/SubjectRepository.java
@@ -1,4 +1,14 @@
 package com.example.uspForum.repository;
 
-public interface SubjectRepository {
+import com.example.uspForum.model.Subject;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface SubjectRepository extends CrudRepository<Subject, Long> {
+
+    @Query("SELECT * FROM subject WHERE abbreviation = :abbreviation")
+    List<Subject> findSubjectByAbbreviation(String abbreviation);
+
 }

a5901578530048bd3591ca3df145996bc359224d
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 00:37:08 -0300

    Add findByTeacherName method

--- src/main/java/com/example/uspForum/repository/TeacherRepository.java
+++ src/main/java/com/example/uspForum/repository/TeacherRepository.java
@@ -1,4 +1,14 @@
 package com.example.uspForum.repository;
 
-public interface TeacherRepository {
+import com.example.uspForum.model.Teacher;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TeacherRepository extends CrudRepository<Teacher, Long> {
+
+    @Query("SELECT * FROM teacher WHERE name = :teacherName")
+    List<Teacher> findByTeacherName(String teacherName);
+
 }

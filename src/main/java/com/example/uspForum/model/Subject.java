d4ea8c70813c65f7c19d60b634f1f3c0062daab6
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 11:24:22 -0300

    Rename createdByUserId field to createdByAccountId

--- src/main/java/com/example/uspForum/model/Subject.java
+++ src/main/java/com/example/uspForum/model/Subject.java
@@ -12,6 +12,6 @@ public class Subject {
     private String code;
     private String relatedCourse;
     private long teacherId;
-    private long createdByUserId;
+    private long createdByAccountId;
 
 }

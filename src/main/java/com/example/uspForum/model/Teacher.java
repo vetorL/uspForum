2526f51e38517f5db202fe0f7f7f6796f277fe4f
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 16:22:24 -0300

    Generate getters and setters for Teacher

--- src/main/java/com/example/uspForum/model/Teacher.java
+++ src/main/java/com/example/uspForum/model/Teacher.java
@@ -11,4 +11,35 @@ public class Teacher {
     private String email;
     private String teachesAt;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTeachesAt() {
+        return teachesAt;
+    }
+
+    public void setTeachesAt(String teachesAt) {
+        this.teachesAt = teachesAt;
+    }
 }

bce588a76a14c930e3f59ab2fdf30fd211b78c09
vetorL <145482274+vetorL@users.noreply.github.com>
2024-05-31 00:10:18 -0300

    Implement Teacher model

--- src/main/java/com/example/uspForum/model/Teacher.java
+++ src/main/java/com/example/uspForum/model/Teacher.java
@@ -1,4 +1,13 @@
 package com.example.uspForum.model;
 
+import org.springframework.data.annotation.Id;
+
 public class Teacher {
+
+    @Id
+    private long id;
+
+    private String name;
+    private String email;
+
 }

7e8ec22cd5948e5b82a39e973ef626ab615367c5
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-02 11:28:53 -0300

    Implement getCreateSubject

--- src/main/java/com/example/uspForum/controller/CreateSubjectController.java
+++ src/main/java/com/example/uspForum/controller/CreateSubjectController.java
@@ -1,4 +1,14 @@
 package com.example.uspForum.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CreateSubjectController {
+
+    @GetMapping("/criar")
+    public String getCreateSubject() {
+        return "create-subject.html";
+    }
+
 }

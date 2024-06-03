08c025fca7baee1c2e1a0308d97620e2c0909679
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-03 14:27:09 -0300

    Adapt to account table removal

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,4 @@
-INSERT INTO account (id, username, password)
-VALUES (1, 'admin', '123');
-
 INSERT INTO subject (id, name, abbreviation, code, related_course,
-                     teacher_name, created_by_account_id )
+                     teacher_name )
 VALUES (1, 'Introdução à Administração e Economia para Computação',
-        'IAEC', 'ACH2053', 'Sistemas de Informação', 'Violeta Sun', 1);
+        'IAEC', 'ACH2053', 'Sistemas de Informação', 'Violeta Sun');

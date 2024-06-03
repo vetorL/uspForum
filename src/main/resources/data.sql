ba7b5a6b3ff622034411356dfa9e75f5b518b3f8
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-03 14:15:29 -0300

    Delete subject_with_teacher_name view

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,17 +8,3 @@ INSERT INTO subject (id, name, abbreviation, code, related_course,
                      teacher_id, created_by_account_id )
 VALUES (1, 'Introdução à Administração e Economia para Computação',
         'IAEC', 'ACH2053', 'Sistemas de Informação', 1, 1);
-
-CREATE OR REPLACE VIEW subject_with_teacher_name AS
-    SELECT
-        subject.id,
-        subject.name,
-        subject.abbreviation,
-        subject.code,
-        subject.related_course,
-        subject.teacher_id,
-        subject.created_by_account_id,
-        teacher.name AS teacher_name
-    FROM
-        subject
-        INNER JOIN teacher ON teacher.id = subject.teacher_id;

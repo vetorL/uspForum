766e42877a5a4081f43f2def58b5937a495570d1
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-03 15:58:22 -0300

    Implement postCreateSubject

--- src/main/java/com/example/uspForum/controller/CreateSubjectController.java
+++ src/main/java/com/example/uspForum/controller/CreateSubjectController.java
@@ -5,6 +5,8 @@ import com.example.uspForum.service.CreateSubjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CreateSubjectController {
@@ -18,6 +20,25 @@ public class CreateSubjectController {
     @GetMapping("/criar")
     public String getCreateSubject(Model model) {
         model.addAttribute("subject", new Subject());
+        return "create-subject.html";
+    }
+
+    @PostMapping("/criar")
+    public String postCreateSubject(
+            @ModelAttribute Subject subject,
+            Model model
+    ) {
+
+        boolean result = createSubjectService.createSubject(subject);
+
+        if (result) {
+            model.addAttribute("message",
+                    "Matéria criada com sucesso!");
+        } else {
+            model.addAttribute("message",
+                    "Falha na tentativa, preencha todos os campos!");
+        }
+
         return "create-subject.html";
     }
 

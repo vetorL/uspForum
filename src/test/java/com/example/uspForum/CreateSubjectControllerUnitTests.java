d88b9fccae17274f6a097ad31e75ce70018f17e1
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-03 14:47:00 -0300

    Mock CreateSubjectService

--- src/test/java/com/example/uspForum/CreateSubjectControllerUnitTests.java
+++ src/test/java/com/example/uspForum/CreateSubjectControllerUnitTests.java
@@ -1,16 +1,27 @@
 package com.example.uspForum;
 
 import com.example.uspForum.controller.CreateSubjectController;
+import com.example.uspForum.service.CreateSubjectService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
 public class CreateSubjectControllerUnitTests {
+
+    @Mock
+    private CreateSubjectService createSubjectService;
+
+    @InjectMocks
+    private CreateSubjectController createSubjectController;
 
     @Test
     public void getCreateSubjectReturnsCorrectHtml() {
-        CreateSubjectController controller = new CreateSubjectController();
-        assertEquals("create-subject.html", controller.getCreateSubject());
+        assertEquals("create-subject.html", createSubjectController.getCreateSubject());
     }
 
 }

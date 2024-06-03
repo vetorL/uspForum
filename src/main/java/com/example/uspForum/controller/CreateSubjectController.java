bd0bfdef58b38e07236707b8e6d4408d9fe230fd
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-03 15:57:17 -0300

    Inject CreateSubjectService

--- src/main/java/com/example/uspForum/controller/CreateSubjectController.java
+++ src/main/java/com/example/uspForum/controller/CreateSubjectController.java
@@ -1,10 +1,19 @@
 package com.example.uspForum.controller;
 
+import com.example.uspForum.model.Subject;
+import com.example.uspForum.service.CreateSubjectService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CreateSubjectController {
+
+    public CreateSubjectService createSubjectService;
+
+    public CreateSubjectController(CreateSubjectService createSubjectService) {
+        this.createSubjectService = createSubjectService;
+    }
 
     @GetMapping("/criar")
     public String getCreateSubject() {

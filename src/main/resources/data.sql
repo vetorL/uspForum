63db35394d36d5dba7a1acfb688d23055a8ea5cb
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-03 15:53:23 -0300

    Make first id be 0

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO subject (id, name, abbreviation, code, related_course,
                      teacher_name )
-VALUES (1, 'Introdução à Administração e Economia para Computação',
+VALUES (0, 'Introdução à Administração e Economia para Computação',
         'IAEC', 'ACH2063', 'Sistemas de Informação', 'Violeta Sun');

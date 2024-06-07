6670899816c61adc82f8378be6c654c880e4292e
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-07 20:22:40 -0300

    Remove query annotations

--- src/main/java/com/example/uspForum/repository/SubjectRepository.java
+++ src/main/java/com/example/uspForum/repository/SubjectRepository.java
@@ -1,23 +1,18 @@
 package com.example.uspForum.repository;
 
 import com.example.uspForum.model.Subject;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface SubjectRepository extends CrudRepository<Subject, Long> {
 
-    @Query("SELECT * FROM subject WHERE abbreviation = :abbreviation")
     List<Subject> findSubjectByAbbreviation(String abbreviation);
 
-    @Query("SELECT * FROM subject WHERE code = :code")
     List<Subject> findSubjectByCode(String code);
 
-    @Query("SELECT * FROM subject WHERE name = :name")
     List<Subject> findSubjectByName(String name);
 
-    @Query("SELECT * FROM subject WHERE teacher_name = :teacherName")
     List<Subject> findSubjectByTeacherName(String teacherName);
 
 }

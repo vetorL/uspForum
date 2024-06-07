0d20bf32db0deceb87a7b22d6a9e8d2e5504a3ad
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-07 20:17:09 -0300

    Remove data.sql

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO subject (id, name, abbreviation, code, related_course,
-                     teacher_name )
-VALUES (0, 'Introdução à Administração e Economia para Computação',
-        'IAEC', 'ACH2063', 'Sistemas de Informação', 'Violeta Sun');

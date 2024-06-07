e20515b1d350a1edd86b1852f7d6b81c8c23a318
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-07 20:17:24 -0300

    Add dataLoader CommandLineRunner bean

--- src/main/java/com/example/uspForum/UspForumApplication.java
+++ src/main/java/com/example/uspForum/UspForumApplication.java
@@ -1,13 +1,24 @@
 package com.example.uspForum;
 
+import com.example.uspForum.model.Subject;
+import com.example.uspForum.repository.SubjectRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UspForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UspForumApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner dataLoader(SubjectRepository repo) {
+		return args -> {
+			repo.save(new Subject(0, "Introdução à Administração e Economia para Computação","IAEC", "ACH2063", "Sistemas de Informação", "Violeta Sun"));
+		};
 	}
 
 }

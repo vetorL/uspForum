b1151a6ca725d3805b77a6510dda099ed5412e64
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-08 15:01:17 -0300

    Adapt application to Spring Data JPA

--- src/main/java/com/example/uspForum/model/Subject.java
+++ src/main/java/com/example/uspForum/model/Subject.java
@@ -1,11 +1,13 @@
 package com.example.uspForum.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class Subject {

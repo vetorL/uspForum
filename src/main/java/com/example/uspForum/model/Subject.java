0bfb67ae03002705f2467704576eb59728eec8ce
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-08 11:07:11 -0300

    Annotate Subject with lombok

--- src/main/java/com/example/uspForum/model/Subject.java
+++ src/main/java/com/example/uspForum/model/Subject.java
@@ -1,7 +1,13 @@
 package com.example.uspForum.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Subject {
 
     @Id
@@ -12,67 +18,5 @@ public class Subject {
     private String code;
     private String relatedCourse;
     private String teacherName;
-
-    public Subject() {
-
-    }
-
-    public Subject(long id, String name, String abbreviation, String code,
-                   String relatedCourse, String teacherName) {
-        this.id = id;
-        this.name = name;
-        this.abbreviation = abbreviation;
-        this.code = code;
-        this.relatedCourse = relatedCourse;
-        this.teacherName = teacherName;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAbbreviation() {
-        return abbreviation;
-    }
-
-    public void setAbbreviation(String abbreviation) {
-        this.abbreviation = abbreviation;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getRelatedCourse() {
-        return relatedCourse;
-    }
-
-    public void setRelatedCourse(String relatedCourse) {
-        this.relatedCourse = relatedCourse;
-    }
-
-    public void setTeacherName(String teacherName) {
-        this.teacherName = teacherName;
-    }
-
-    public String getTeacherName() {
-        return teacherName;
-    }
 
 }

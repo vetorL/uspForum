41ac3586c65b8e71966c551732a33c177ff8b270
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-09 13:49:23 -0300

    Annotate id with @GeneratedValue

--- src/main/java/com/example/uspForum/model/Subject.java
+++ src/main/java/com/example/uspForum/model/Subject.java
@@ -1,6 +1,8 @@
 package com.example.uspForum.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +15,7 @@ import lombok.NoArgsConstructor;
 public class Subject {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private String name;

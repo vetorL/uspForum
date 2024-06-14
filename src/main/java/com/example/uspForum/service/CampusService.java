c860663d8fa7048eabf57144e5ac614456a92b81
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-14 14:16:05 -0300

    Create CampusService

---
+++ src/main/java/com/example/uspForum/service/CampusService.java
@@ -0,0 +1,25 @@
+package com.example.uspForum.service;
+
+import com.example.uspForum.model.Campus;
+import com.example.uspForum.repository.CampusRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CampusService {
+
+    private final CampusRepository campusRepository;
+
+    public CampusService(CampusRepository campusRepository) {
+        this.campusRepository = campusRepository;
+    }
+
+    public List<Campus> findAll() {
+        List<Campus> campi = new ArrayList<>();
+        campusRepository.findAll().forEach(campi::add);
+        return campi;
+    }
+
+}

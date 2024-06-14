570dcd279befb4cc6fce7ed3381f884631a3fa6d
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-14 19:55:36 -0300

    Adapt to use SubjectCreationDTO

--- src/main/java/com/example/uspForum/controller/CreateSubjectController.java
+++ src/main/java/com/example/uspForum/controller/CreateSubjectController.java
@@ -1,6 +1,9 @@
 package com.example.uspForum.controller;
 
+import com.example.uspForum.model.Campus;
+import com.example.uspForum.model.Mapper;
 import com.example.uspForum.model.Subject;
+import com.example.uspForum.model.SubjectCreationDTO;
 import com.example.uspForum.service.CampusService;
 import com.example.uspForum.service.CreateSubjectService;
 import org.springframework.stereotype.Controller;
@@ -14,24 +17,31 @@ public class CreateSubjectController {
 
     private final CreateSubjectService createSubjectService;
     private final CampusService campusService;
+    private final Mapper mapper;
 
-    public CreateSubjectController(CreateSubjectService createSubjectService, CampusService campusService) {
+    public CreateSubjectController(CreateSubjectService createSubjectService, CampusService campusService, Mapper mapper) {
         this.createSubjectService = createSubjectService;
         this.campusService = campusService;
+        this.mapper = mapper;
     }
 
     @GetMapping("/criar")
     public String getCreateSubject(Model model) {
-        model.addAttribute("subject", new Subject());
+        model.addAttribute("subjectCreationDTO", new SubjectCreationDTO());
         model.addAttribute("campi", campusService.findAll());
         return "create-subject.html";
     }
 
     @PostMapping("/criar")
     public String postCreateSubject(
-            @ModelAttribute Subject subject,
+            @ModelAttribute SubjectCreationDTO subjectCreationDTO,
             Model model
     ) {
+
+        Campus campus = campusService.findById(
+                Long.parseLong(subjectCreationDTO.getRelatedCampusId())
+        );
+        Subject subject = mapper.toSubject(subjectCreationDTO, campus);
 
         boolean result = createSubjectService.createSubject(subject);
 

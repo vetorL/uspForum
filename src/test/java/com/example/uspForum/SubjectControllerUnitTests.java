99dc4e860e583bd17141dfd9664208443a93e863
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-14 20:47:08 -0300

    Implement testGetSubjectById

---
+++ src/test/java/com/example/uspForum/SubjectControllerUnitTests.java
@@ -0,0 +1,48 @@
+package com.example.uspForum;
+
+import com.example.uspForum.model.Campus;
+import com.example.uspForum.model.Subject;
+import com.example.uspForum.service.SubjectService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SubjectControllerUnitTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SubjectService subjectService;
+
+    @Test
+    void testGetSubjectById() throws Exception {
+        long subjectId = 1L;
+
+        Subject subject = new Subject(subjectId, "Test Subject", "TS", "TCH4839", "Testing",
+                new Campus(1, "TST", "Testing Test"), "Testor");
+        Optional<Subject> subjectOptional = Optional.of(subject);
+
+        when(subjectService.findSubjectById(subjectId)).thenReturn(subjectOptional);
+
+        this.mockMvc.perform(get("/disciplina/" + subjectId))
+                .andExpect(status().isOk())
+                .andExpect(view().name("subject.html"))
+                .andExpect(content().string(containsString("Test Subject")));
+    }
+
+}

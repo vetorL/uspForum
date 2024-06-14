e4803ca392adca7eaef6cce7119d8c38837c5ec8
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-14 19:54:07 -0300

    Implement SubjectCreationDTO

--- src/main/java/com/example/uspForum/model/SubjectCreationDTO.java
+++ src/main/java/com/example/uspForum/model/SubjectCreationDTO.java
@@ -1,4 +1,19 @@
 package com.example.uspForum.model;
 
-public class SubjectFormDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubjectCreationDTO {
+
+    private String name;
+    private String abbreviation;
+    private String code;
+    private String relatedCourse;
+    private String relatedCampusId;
+    private String teacherName;
+
 }

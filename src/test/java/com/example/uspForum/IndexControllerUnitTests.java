509c9b958bd3b7cab4145c094a0d81b04cf49012
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-14 20:27:35 -0300

    Make IndexControllerUnitTests use MockMvc

--- src/test/java/com/example/uspForum/IndexControllerUnitTests.java
+++ src/test/java/com/example/uspForum/IndexControllerUnitTests.java
@@ -1,17 +1,27 @@
 package com.example.uspForum;
 
-import com.example.uspForum.controller.IndexController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class IndexControllerUnitTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    public void getIndexReturnsCorrectHtml() {
-        IndexController indexController = new IndexController();
-        String returnedHtml = indexController.index();
-        assertEquals("index.html", returnedHtml);
+    public void getIndexTest() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index.html"));
     }
 
 }

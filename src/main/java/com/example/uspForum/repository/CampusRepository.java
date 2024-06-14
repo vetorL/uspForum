b4325aebff678694a17d669e0147ce722a333541
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-14 13:43:52 -0300

    Implement CampusRepository

---
+++ src/main/java/com/example/uspForum/repository/CampusRepository.java
@@ -0,0 +1,7 @@
+package com.example.uspForum.repository;
+
+import com.example.uspForum.model.Campus;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CampusRepository extends CrudRepository<Campus, Long> {
+}

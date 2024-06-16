58fff0a99dc8193c46bd661408ee7b7ee1c9ae6e
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-16 19:27:15 -0300

    Add @OneToMany annotated field of subjectReviews

--- src/main/java/com/example/uspForum/model/Subject.java
+++ src/main/java/com/example/uspForum/model/Subject.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Entity
 @AllArgsConstructor
@@ -24,5 +26,8 @@ public class Subject {
     private Campus relatedCampus;
 
     private String teacherName;
+
+    @OneToMany
+    private List<SubjectReview> subjectReviews;
 
 }

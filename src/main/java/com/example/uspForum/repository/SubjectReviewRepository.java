5b429c447043c5374a330ad4762b6bba704f61bd
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-16 19:28:36 -0300

    Create SubjectReviewRepository

---
+++ src/main/java/com/example/uspForum/repository/SubjectReviewRepository.java
@@ -0,0 +1,7 @@
+package com.example.uspForum.repository;
+
+import com.example.uspForum.model.SubjectReview;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubjectReviewRepository extends CrudRepository<SubjectReview, Long> {
+}

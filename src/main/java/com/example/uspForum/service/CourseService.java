53dfb679b06b2ea2ded99aae509a6c5c527779bb
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-16 23:26:37 -0300

    Add CourseService and implement getAllCourses

---
+++ src/main/java/com/example/uspForum/service/CourseService.java
@@ -0,0 +1,25 @@
+package com.example.uspForum.service;
+
+import com.example.uspForum.model.Course;
+import com.example.uspForum.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public List<Course> getAllCourses() {
+        List<Course> courses = new ArrayList<>();
+        courseRepository.findAll().forEach(courses::add);
+        return courses;
+    }
+
+}

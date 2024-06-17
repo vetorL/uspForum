a267cc760b8a94e3827c4d491156bbaf70c76d69
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-16 22:18:28 -0300

    Create ProfessorRepository

---
+++ src/main/java/com/example/uspForum/repository/ProfessorRepository.java
@@ -0,0 +1,10 @@
+package com.example.uspForum.repository;
+
+import com.example.uspForum.model.Professor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProfessorRepository extends CrudRepository<Professor, Long> {
+
+    Professor findByEmail(String email);
+
+}

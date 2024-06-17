ecc3b16c30dc01f280fa137058740ec857133709
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-16 22:18:15 -0300

    Create Professor model

---
+++ src/main/java/com/example/uspForum/model/Professor.java
@@ -0,0 +1,24 @@
+package com.example.uspForum.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public String name;
+    public String email;
+
+}

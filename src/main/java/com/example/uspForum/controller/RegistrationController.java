9ed2c129640266e16e963eea8cf66279bf13effb
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-17 13:51:14 -0300

    Implement registerForm

--- src/main/java/com/example/uspForum/controller/RegistrationController.java
+++ src/main/java/com/example/uspForum/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package com.example.uspForum.controller;
 import com.example.uspForum.repository.CustomUserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +16,11 @@ public class RegistrationController {
     public RegistrationController(CustomUserRepository customUserRepository, PasswordEncoder passwordEncoder) {
         this.customUserRepository = customUserRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping
+    public String registerForm() {
+        return "registration";
     }
 
 }

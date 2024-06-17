419c3dd3237009d4c305f7c6b64fc4c0a0d84d8b
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-17 13:50:22 -0300

    Inject CustomUserRepository and PasswordEncoder

--- src/main/java/com/example/uspForum/controller/RegistrationController.java
+++ src/main/java/com/example/uspForum/controller/RegistrationController.java
@@ -1,9 +1,20 @@
 package com.example.uspForum.controller;
 
+import com.example.uspForum.repository.CustomUserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/registrar")
 public class RegistrationController {
+
+    private CustomUserRepository customUserRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public RegistrationController(CustomUserRepository customUserRepository, PasswordEncoder passwordEncoder) {
+        this.customUserRepository = customUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
 }

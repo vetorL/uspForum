8c34bdcfdc5d057c5d884cbb35b296035c627b66
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-16 22:22:17 -0300

    Implement findAll method

--- src/main/java/com/example/uspForum/service/ProfessorService.java
+++ src/main/java/com/example/uspForum/service/ProfessorService.java
@@ -4,6 +4,7 @@ import com.example.uspForum.model.Professor;
 import com.example.uspForum.repository.ProfessorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,6 +18,12 @@ public class ProfessorService {
 
     public Professor findByEmail(String email) {
         return professorRepository.findByEmail(email);
+    }
+
+    public List<Professor> findAll() {
+        List<Professor> professors = new ArrayList<>();
+        professorRepository.findAll().forEach(professors::add);
+        return professors;
     }
 
 }

7d9cd92f55475a94a5ba811095da3338465b973f
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-16 23:45:55 -0300

    Add findByNameAndCampusAbbreviation method

--- src/main/java/com/example/uspForum/repository/CourseRepository.java
+++ src/main/java/com/example/uspForum/repository/CourseRepository.java
@@ -3,5 +3,10 @@ package com.example.uspForum.repository;
 import com.example.uspForum.model.Course;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends CrudRepository<Course, Long> {
+
+    List<Course> findByNameAndCampusAbbreviation(String name, String campusAbbreviation);
+
 }

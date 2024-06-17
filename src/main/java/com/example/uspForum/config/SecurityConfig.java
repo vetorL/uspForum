d83393451716cc8c8fbbf69162350640e2e3f387
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-17 13:47:33 -0300

    Create UserDetailsService bean

--- src/main/java/com/example/uspForum/config/SecurityConfig.java
+++ src/main/java/com/example/uspForum/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.example.uspForum.config;
 
+import com.example.uspForum.model.CustomUser;
+import com.example.uspForum.repository.CustomUserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,6 +15,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(CustomUserRepository customUserRepository) {
+        return username -> {
+            CustomUser customUser = customUserRepository.findByUsername(username);
+            if (customUser != null) {
+                return customUser;
+            }
+
+            throw new UsernameNotFoundException("Usuário '" + username + "' não encontrado");
+        };
     }
 
 }

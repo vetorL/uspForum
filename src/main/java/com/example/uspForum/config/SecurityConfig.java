3c891ceafbca30f97e50fd5d9d7565ebe2b3d9ff
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-17 14:26:28 -0300

    Make Spring Security ignore h2-console when dev

--- src/main/java/com/example/uspForum/config/SecurityConfig.java
+++ src/main/java/com/example/uspForum/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import com.example.uspForum.model.CustomUser;
 import com.example.uspForum.repository.CustomUserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -43,6 +46,12 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Profile("dev")
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
     }
 
 }

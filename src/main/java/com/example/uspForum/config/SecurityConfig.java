9189a699a058049648ed7a6dfa94a627898b1384
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-17 14:13:41 -0300

    Add SecurityFilterChain bean

--- src/main/java/com/example/uspForum/config/SecurityConfig.java
+++ src/main/java/com/example/uspForum/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.example.uspForum.model.CustomUser;
 import com.example.uspForum.repository.CustomUserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -27,6 +29,16 @@ public class SecurityConfig {
 
             throw new UsernameNotFoundException("Usuário '" + username + "' não encontrado");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/criar").hasAuthority("USER")
+                        .anyRequest().permitAll());
+
+        return http.build();
     }
 
 }

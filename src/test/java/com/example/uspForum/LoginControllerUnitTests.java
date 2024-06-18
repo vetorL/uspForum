5fe32caa0f79da30233e73c14324b8302ff45f85
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-17 23:32:48 -0300

    Add testGetLogin test

---
+++ src/test/java/com/example/uspForum/LoginControllerUnitTests.java
@@ -0,0 +1,27 @@
+package com.example.uspForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerUnitTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetLogin() throws Exception {
+        this.mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+}

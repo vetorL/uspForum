eae88ef016f9b0a5437fa30fec1d6949ddf66f07
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-18 09:54:19 -0300

    Add testPostRegisterForm test

--- src/test/java/com/example/uspForum/RegistrationControllerUnitTests.java
+++ src/test/java/com/example/uspForum/RegistrationControllerUnitTests.java
@@ -1,18 +1,31 @@
 package com.example.uspForum;
 
+import com.example.uspForum.model.CustomUser;
+import com.example.uspForum.model.RegistrationFormDTO;
+import com.example.uspForum.repository.CustomUserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RegistrationControllerUnitTests {
+
+    @MockBean
+    private CustomUserRepository customUserRepository;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private MockMvc mockMvc;
@@ -22,6 +35,18 @@ public class RegistrationControllerUnitTests {
         this.mockMvc.perform(get("/registrar"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("registration"));
+    }
+
+    @Test
+    void testPostRegisterForm() throws Exception {
+
+        RegistrationFormDTO form = new RegistrationFormDTO();
+
+        when(customUserRepository.save(form.toCustomUser(passwordEncoder))).thenReturn(new CustomUser());
+
+        this.mockMvc.perform(post("/registrar").with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
     }
 
 }

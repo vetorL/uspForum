3b7a28509a199978723818744ca4d1d3e92c8ed5
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-20 13:08:46 -0300

    Add postar POST route

--- src/main/java/com/example/uspForum/controller/SubjectController.java
+++ src/main/java/com/example/uspForum/controller/SubjectController.java
@@ -1,12 +1,14 @@
 package com.example.uspForum.controller;
 
+import com.example.uspForum.model.CustomUser;
 import com.example.uspForum.model.Subject;
+import com.example.uspForum.model.SubjectReview;
+import com.example.uspForum.model.SubjectReviewDTO;
 import com.example.uspForum.service.SubjectService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -26,11 +28,30 @@ public class SubjectController {
 
         if(subject.isPresent()) {
             model.addAttribute("subject", subject.get());
+            model.addAttribute("subjectReviewDTO", new SubjectReviewDTO());
         } else {
             return "redirect:/";
         }
 
         return "subject.html";
+    }
+
+    @PostMapping("/postar/{id}")
+    public String postSubjectReview(@PathVariable("id") Long id,
+                                    @ModelAttribute SubjectReviewDTO subjectReviewDTO,
+                                    @AuthenticationPrincipal CustomUser author) {
+
+        Optional<Subject> subject = subjectService.findSubjectById(id);
+
+        if(subject.isPresent()) {
+            subjectService.postSubjectReview(
+                    subjectReviewDTO.toSubjectReview(author, subject.get())
+            );
+            return "redirect:/disciplina/" + subject.get().getId();
+        } else {
+            return "redirect:/";
+        }
+
     }
 
 }

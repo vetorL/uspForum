f6562b01ff4f18292d544b5e6840a426e69fb204
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-20 14:49:04 -0300

    Display heroes in index page

--- src/main/java/com/example/uspForum/controller/IndexController.java
+++ src/main/java/com/example/uspForum/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.example.uspForum.controller;
 
+import com.example.uspForum.repository.CustomUserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final CustomUserRepository customUserRepository;
+
+    public IndexController(CustomUserRepository customUserRepository) {
+        this.customUserRepository = customUserRepository;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("heroes", customUserRepository.findAll());
         return "index.html";
     }
 

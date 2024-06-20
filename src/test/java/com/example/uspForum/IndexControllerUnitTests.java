ea427630eaff0a7f7a5c1a1436c060feddf76004
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-20 17:14:29 -0300

    Mock CustomUserRepository

--- src/test/java/com/example/uspForum/IndexControllerUnitTests.java
+++ src/test/java/com/example/uspForum/IndexControllerUnitTests.java
@@ -1,11 +1,16 @@
 package com.example.uspForum;
 
+import com.example.uspForum.repository.CustomUserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -14,11 +19,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class IndexControllerUnitTests {
 
+    @MockBean
+    private CustomUserRepository customUserRepository;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void getIndexTest() throws Exception {
+        when(customUserRepository.findAll()).thenReturn(new ArrayList<>());
+
         this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index.html"));

d5826da6ba3f643a43eb33ba3d8e74658e782197
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-23 16:15:22 -0300

    Implement postSubjectReviewVote endpoint

--- src/main/java/com/example/uspForum/controller/SubjectController.java
+++ src/main/java/com/example/uspForum/controller/SubjectController.java
@@ -1,12 +1,12 @@
 package com.example.uspForum.controller;
 
-import com.example.uspForum.model.CustomUser;
-import com.example.uspForum.model.Subject;
-import com.example.uspForum.model.SubjectReview;
-import com.example.uspForum.model.SubjectReviewDTO;
+import com.example.uspForum.model.*;
+import com.example.uspForum.service.SubjectReviewService;
 import com.example.uspForum.service.SubjectService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,9 +17,11 @@ import java.util.Optional;
 public class SubjectController {
 
     private final SubjectService subjectService;
+    private final SubjectReviewService subjectReviewService;
 
-    public SubjectController(SubjectService subjectService) {
+    public SubjectController(SubjectService subjectService, SubjectReviewService subjectReviewService) {
         this.subjectService = subjectService;
+        this.subjectReviewService = subjectReviewService;
     }
 
     @GetMapping("{id}")
@@ -29,11 +31,47 @@ public class SubjectController {
         if(subject.isPresent()) {
             model.addAttribute("subject", subject.get());
             model.addAttribute("subjectReviewDTO", new SubjectReviewDTO());
+            model.addAttribute("voteDTO", new VoteDTO());
         } else {
             return "redirect:/";
         }
 
         return "subject.html";
+    }
+
+    @PostMapping("/votar")
+    @Transactional
+    public String postSubjectReviewVote(@RequestHeader(value = HttpHeaders.REFERER, required = false) final String referrer,
+                                      @ModelAttribute  VoteDTO voteDTO,
+                                      @AuthenticationPrincipal CustomUser voter) {
+
+        SubjectReview subjectReview = subjectReviewService.findById(voteDTO.getSubjectReviewId());
+
+        Vote vote = voteDTO.toVote(voter, subjectReview);
+
+        // if user already voted remove vote from database
+        if(subjectReviewService.userAlreadyVotedOnReview(voter, subjectReview)) {
+            // identify vote type
+            int value = subjectReviewService.getUserReviewVoteType(voter, subjectReview);
+
+            // if of the same type remove it from database
+            if(value == vote.getVote()) {
+                subjectReviewService.removeVoteFromReview(voter, subjectReview);
+            }
+
+            // if not of the same type then update it
+            else if (value != vote.getVote()) {
+                subjectReviewService.removeVoteFromReview(voter, subjectReview);
+                subjectReviewService.addVoteToReview(vote);
+            }
+
+        } else {
+            // else add vote to database
+            subjectReviewService.addVoteToReview(vote);
+        }
+
+        return "redirect:" + referrer;
+
     }
 
     @PostMapping("/postar/{id}")

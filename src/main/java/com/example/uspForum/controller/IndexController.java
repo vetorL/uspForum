d8602692addd2b104e9967e158f2ad9121cc697c
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-23 16:55:09 -0300

    Implement /{campus} endpoint

--- src/main/java/com/example/uspForum/controller/IndexController.java
+++ src/main/java/com/example/uspForum/controller/IndexController.java
@@ -1,18 +1,21 @@
 package com.example.uspForum.controller;
 
-import com.example.uspForum.repository.CustomUserRepository;
+import com.example.uspForum.service.CampusService;
 import com.example.uspForum.service.CustomUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
     private final CustomUserService customUserService;
+    private final CampusService campusService;
 
-    public IndexController(CustomUserService customUserService) {
+    public IndexController(CustomUserService customUserService, CampusService campusService) {
         this.customUserService = customUserService;
+        this.campusService = campusService;
     }
 
     @GetMapping("/")
@@ -20,5 +23,12 @@ public class IndexController {
         model.addAttribute("heroes", customUserService.findAllOrderByRepDesc());
         return "index.html";
     }
+
+    @GetMapping("/{campus}")
+    public String getCampus(@PathVariable("campus") String campusAbbreviation, Model model) {
+        model.addAttribute("campus", campusService.findByAbbreviation(campusAbbreviation));
+        return "campus.html";
+    }
+
 
 }

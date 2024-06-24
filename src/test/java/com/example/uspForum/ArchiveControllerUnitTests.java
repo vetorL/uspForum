5152e58c83cfb68076f7e16b727cf4f29c3035aa
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-24 00:27:05 -0300

    Add getCourseTest and fix getCampusTest
    getCampusTest now calls /arquivo/ instead of just /

--- src/test/java/com/example/uspForum/ArchiveControllerUnitTests.java
+++ src/test/java/com/example/uspForum/ArchiveControllerUnitTests.java
@@ -1,7 +1,9 @@
 package com.example.uspForum;
 
 import com.example.uspForum.model.Campus;
+import com.example.uspForum.model.Course;
 import com.example.uspForum.service.CampusService;
+import com.example.uspForum.service.CourseService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,6 +23,9 @@ public class ArchiveControllerUnitTests {
     @MockBean
     private CampusService campusService;
 
+    @MockBean
+    private CourseService courseService;
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -31,9 +36,24 @@ public class ArchiveControllerUnitTests {
 
         when(campusService.findByAbbreviation("TEST")).thenReturn(campus);
 
-        this.mockMvc.perform(get("/" + campus.getAbbreviation()))
+        this.mockMvc.perform(get("/arquivo/" + campus.getAbbreviation()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("campus.html"));
+    }
+
+    @Test
+    public void getCourseTest() throws Exception {
+        Campus campus = new Campus("tesing institution", "TEST");
+        Course course = new Course("test name", "test-name", campus);
+
+        String testURL = "/arquivo/" + campus.getAbbreviation() + "/" + course.getNormalizedName();
+
+        when(courseService.findByNormalizedNameAndCampusAbbreviation(
+                course.getNormalizedName(), campus.getAbbreviation())).thenReturn(course);
+
+        this.mockMvc.perform(get(testURL))
+                .andExpect(status().isOk())
+                .andExpect(view().name("course.html"));
     }
 
 }

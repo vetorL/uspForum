a4591a984050c1a70f2f4defd76f687554c920e2
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-24 11:08:18 -0300

    Implement getSubjectTest

--- src/test/java/com/example/uspForum/ArchiveControllerUnitTests.java
+++ src/test/java/com/example/uspForum/ArchiveControllerUnitTests.java
@@ -2,14 +2,19 @@ package com.example.uspForum;
 
 import com.example.uspForum.model.Campus;
 import com.example.uspForum.model.Course;
+import com.example.uspForum.model.Professor;
+import com.example.uspForum.model.Subject;
 import com.example.uspForum.service.CampusService;
 import com.example.uspForum.service.CourseService;
+import com.example.uspForum.service.SubjectService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,6 +30,9 @@ public class ArchiveControllerUnitTests {
 
     @MockBean
     private CourseService courseService;
+
+    @MockBean
+    private SubjectService subjectService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -54,6 +62,24 @@ public class ArchiveControllerUnitTests {
         this.mockMvc.perform(get(testURL))
                 .andExpect(status().isOk())
                 .andExpect(view().name("course.html"));
+    }
+
+    @Test
+    void getSubjectTest() throws Exception {
+        Campus campus = new Campus("tesing institution", "TEST");
+        Course course = new Course("test name", "test-name", campus);
+        Subject subject = new Subject("testing", "TST", "T438902", course, new Professor());
+
+        String testURL = "/arquivo/" + campus.getAbbreviation() + "/"
+                + course.getNormalizedName() + "/" + subject.getAbbreviation();
+
+        when(subjectService.findByCourseNormalizedNameAndCourseCampusAbbreviationAndAbbreviation(
+                course.getNormalizedName(), campus.getAbbreviation(), subject.getAbbreviation()))
+                .thenReturn(List.of(subject));
+
+        this.mockMvc.perform(get(testURL))
+                .andExpect(status().isOk())
+                .andExpect(view().name("subject-professor-list.html"));
     }
 
 }

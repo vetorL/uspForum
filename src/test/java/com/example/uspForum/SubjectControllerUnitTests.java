199d62ffc2417976fb52a47575fee44532647be9
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-24 14:04:39 -0300

    Remove testGetSubjectById

--- src/test/java/com/example/uspForum/SubjectControllerUnitTests.java
+++ src/test/java/com/example/uspForum/SubjectControllerUnitTests.java
@@ -1,23 +1,9 @@
 package com.example.uspForum;
 
-import com.example.uspForum.model.Campus;
-import com.example.uspForum.model.Course;
-import com.example.uspForum.model.Professor;
-import com.example.uspForum.model.Subject;
-import com.example.uspForum.service.SubjectService;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Optional;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,26 +11,4 @@ public class SubjectControllerUnitTests {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private SubjectService subjectService;
-
-    @Test
-    void testGetSubjectById() throws Exception {
-        long subjectId = 1L;
-
-        Course course = new Course("Testing", "testing", new Campus("TST", "Testing Test"));
-
-        Subject subject = new Subject("Test Subject", "TS", "TCH4839", course,
-                new Professor("Testor", "testor", "testor@usp.br"));
-        Optional<Subject> subjectOptional = Optional.of(subject);
-
-        when(subjectService.findSubjectById(subjectId)).thenReturn(subjectOptional);
-
-        this.mockMvc.perform(get("/disciplina/" + subjectId))
-                .andExpect(status().isOk())
-                .andExpect(view().name("subject.html"))
-                .andExpect(content().string(containsString("Test Subject")));
-    }
-
 }

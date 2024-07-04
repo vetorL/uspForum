e02e4984020d032d76cac553782f57ec489de284
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-04 15:41:23 -0300

    Remove unused variables and imports

--- src/main/java/com/example/uspForum/controller/SubjectController.java
+++ src/main/java/com/example/uspForum/controller/SubjectController.java
@@ -1,12 +1,10 @@
 package com.example.uspForum.controller;
 
 import com.example.uspForum.model.*;
-import com.example.uspForum.service.SubjectReviewService;
 import com.example.uspForum.service.SubjectService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -16,11 +14,9 @@ import java.util.Optional;
 public class SubjectController {
 
     private final SubjectService subjectService;
-    private final SubjectReviewService subjectReviewService;
 
-    public SubjectController(SubjectService subjectService, SubjectReviewService subjectReviewService) {
+    public SubjectController(SubjectService subjectService) {
         this.subjectService = subjectService;
-        this.subjectReviewService = subjectReviewService;
     }
 
     @PostMapping("/postar/{id}")

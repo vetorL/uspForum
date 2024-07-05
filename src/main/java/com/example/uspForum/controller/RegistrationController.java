2ddcdef6ef891db768fe9a1720ef8b7f2c52f27f
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-05 20:53:38 -0300

    Make processRegistration endpoint validate DTO
    It also changes one of the model attribute's name to registrationFormDTO
    (previously was registerFormDTO)

--- src/main/java/com/example/uspForum/controller/RegistrationController.java
+++ src/main/java/com/example/uspForum/controller/RegistrationController.java
@@ -3,9 +3,11 @@ package com.example.uspForum.controller;
 import com.example.uspForum.model.RegistrationFormDTO;
 import com.example.uspForum.repository.CustomUserRepository;
 import com.example.uspForum.service.CampusService;
+import jakarta.validation.Valid;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,14 +28,20 @@ public class RegistrationController {
 
     @GetMapping
     public String registerForm(Model model) {
-        model.addAttribute("registerFormDTO", new RegistrationFormDTO());
+        model.addAttribute("registrationFormDTO", new RegistrationFormDTO());
         model.addAttribute("campi", campusService.findAll());
         model.addAttribute("title", "Registro");
         return "registration";
     }
 
     @PostMapping
-    public String processRegistration(RegistrationFormDTO form) {
+    public String processRegistration(@Valid RegistrationFormDTO form, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("campi", campusService.findAll());
+            model.addAttribute("title", "Registro");
+            return "registration";
+        }
+
         customUserRepository.save(form.toCustomUser(
                 passwordEncoder, campusService.findByAbbreviation(form.getCampusAbbr())));
         return "redirect:/login";

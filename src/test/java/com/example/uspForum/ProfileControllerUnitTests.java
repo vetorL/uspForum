83294cabe29cebc4c21bc23b206e9576c63910a5
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-05 10:48:52 -0300

    Add testGetProfile

---
+++ src/test/java/com/example/uspForum/ProfileControllerUnitTests.java
@@ -0,0 +1,41 @@
+package com.example.uspForum;
+
+import com.example.uspForum.config.SecurityConfig;
+import com.example.uspForum.controller.ProfileController;
+import com.example.uspForum.model.Campus;
+import com.example.uspForum.model.CustomUser;
+import com.example.uspForum.service.CustomUserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProfileController.class)
+@Import(SecurityConfig.class)
+public class ProfileControllerUnitTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomUserService customUserService;
+
+    @Test
+    @DisplayName("Tests GET request for a user's profile page while unauthenticated")
+    void testGetProfile() throws Exception {
+        when(customUserService.findByUsername(anyString()))
+                .thenReturn(new CustomUser("a@a", "test", "a", new Campus()));
+
+        mockMvc.perform(get("/perfil/{username}", "test"))
+                .andExpect(status().isOk());
+    }
+
+}

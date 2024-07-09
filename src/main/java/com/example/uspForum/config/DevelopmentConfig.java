5a159ede948bb7699ba5561af15b0ef59f1b2818
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-08 23:11:51 -0300

    Add review data
    This one is in the limits of what is allowed in the subjectReview (1000
    chars content and 50 chars title)

--- src/main/java/com/example/uspForum/config/DevelopmentConfig.java
+++ src/main/java/com/example/uspForum/config/DevelopmentConfig.java
@@ -110,7 +110,20 @@ public class DevelopmentConfig {
                             "Foi OK", "O conteudo foi passado com grande rigorosidade, mas atraves de slides.",
                             "Não recomendo"));
             SubjectReview fsiReview =
-                    subjectReviewRepo.save(new SubjectReview(usr, fsi, "STELLAR", "Prova online e tudo mais",
+                    subjectReviewRepo.save(new SubjectReview(usr3, fsi,
+                            "But I must explain to you how all this mistaken id",
+                            "But I must explain to you how all this mistaken idea of denouncing pleasure and " +
+                                    "praising pain was born and I will give you a complete account of the system, and " +
+                                    "expound the actual teachings of the great explorer of the truth, the master-builder " +
+                                    "of human happiness. No one rejects, dislikes, or avoids pleasure itself, because " +
+                                    "it is pleasure, but because those who do not know how to pursue pleasure rationally " +
+                                    "encounter consequences that are extremely painful. Nor again is there anyone who " +
+                                    "loves or pursues or desires to obtain pain of itself, because it is pain, but because " +
+                                    "occasionally circumstances occur in which toil and pain can procure him some great " +
+                                    "pleasure. To take a trivial example, which of us ever undertakes laborious physical " +
+                                    "exercise, except to obtain some advantage from it? But who has any right to find " +
+                                    "fault with a man who chooses to enjoy a pleasure that has no annoying consequences, " +
+                                    "or one who avoids a pain that produces no resultant pleasure? On the other hand, we denounc",
                             "Neutro"));
 
             Vote iaecVote = voteRepository.save(new Vote(-1, usr2, iaecReview));

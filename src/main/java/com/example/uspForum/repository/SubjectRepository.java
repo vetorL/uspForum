51048e57083aa80ba9efde827e50b5c278e118fe
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-10 12:02:13 -0300

    Make search methods return Page

--- src/main/java/com/example/uspForum/repository/SubjectRepository.java
+++ src/main/java/com/example/uspForum/repository/SubjectRepository.java
@@ -1,6 +1,7 @@
 package com.example.uspForum.repository;
 
 import com.example.uspForum.model.Subject;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -17,23 +18,23 @@ public interface SubjectRepository extends CrudRepository<Subject, Long>, Paging
             "LOWER(s.normalizedName) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
             "LOWER(s.course.name) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
             "LOWER(s.professor.name) LIKE LOWER(CONCAT('%', :searchText, '%')) ")
-    List<Subject> findSubjectBySearchText(String searchText, Pageable pageable);
+    Page<Subject> findSubjectBySearchText(String searchText, Pageable pageable);
 
     @Query("SELECT s FROM Subject s WHERE " +
             "LOWER(s.abbreviation) LIKE LOWER(CONCAT('%', :abbreviation, '%'))")
-    List<Subject> findSubjectByAbbreviation(String abbreviation, Pageable pageable);
+    Page<Subject> findSubjectByAbbreviation(String abbreviation, Pageable pageable);
 
     @Query("SELECT s FROM Subject s WHERE " +
             "LOWER(s.code) LIKE LOWER(CONCAT('%', :code, '%'))")
-    List<Subject> findSubjectByCode(String code, Pageable pageable);
+    Page<Subject> findSubjectByCode(String code, Pageable pageable);
 
     @Query("SELECT s FROM Subject s WHERE " +
             "LOWER(s.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-    List<Subject> findSubjectByName(String name, Pageable pageable);
+    Page<Subject> findSubjectByName(String name, Pageable pageable);
 
     @Query("SELECT s FROM Subject s WHERE " +
             "LOWER(s.professor.name) LIKE LOWER(CONCAT('%', :teacherName, '%'))")
-    List<Subject> findSubjectByTeacherName(String teacherName, Pageable pageable);
+    Page<Subject> findSubjectByTeacherName(String teacherName, Pageable pageable);
 
     List<Subject> findByCourseNormalizedNameAndCourseCampusAbbreviationAndAbbreviation(
             String course_normalizedName, String course_campus_abbreviation, String abbreviation);

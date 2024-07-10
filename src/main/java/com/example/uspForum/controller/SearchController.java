ebbc463a177d214320a80bc7b24e579d52df887b
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-10 12:15:41 -0300

    Adapt search to use pagination

--- src/main/java/com/example/uspForum/controller/SearchController.java
+++ src/main/java/com/example/uspForum/controller/SearchController.java
@@ -2,6 +2,7 @@ package com.example.uspForum.controller;
 
 import com.example.uspForum.model.Subject;
 import com.example.uspForum.service.SearchService;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,33 +24,36 @@ public class SearchController {
     public String search(
             @RequestParam String q,
             @RequestParam String t,
+            @RequestParam int p,
             Model model) {
 
-        List<Subject> results = new ArrayList<>();
+        Page<Subject> results = Page.empty();
         String message = "";
 
         if(t.equals("abreviacao-da-disciplina")) {
-            results = searchService.searchSubjectByAbbreviation(q);
+            results = searchService.searchSubjectByAbbreviation(q, p);
         } else if(t.equals("codigo-da-disciplina")) {
-            results = searchService.searchSubjectByCode(q);
+            results = searchService.searchSubjectByCode(q, p);
         } else if(t.equals("nome-da-disciplina")) {
-            results = searchService.searchSubjectByName(q);
+            results = searchService.searchSubjectByName(q, p);
         } else if(t.equals("professor")) {
-            results = searchService.searchSubjectByTeacherName(q);
+            results = searchService.searchSubjectByTeacherName(q, p);
         } else if(t.equals("geral")) {
-            results = searchService.searchSubjectBySearchText(q);
+            results = searchService.searchSubjectBySearchText(q, p);
         }
 
-        if(results.size() == 0) {
+        long totalElements = results.getTotalElements();
+
+        if(totalElements == 0) {
             message = "Não foram encontrados resultados para \'" + q + "\'";
-        } else if(results.size() == 1) {
+        } else if(totalElements == 1) {
             message = "Foi encontrado 1 resultado para \'" + q + "\'";
-        } else if (results.size() > 1) {
-            message = "Foram encontrados " + results.size() + " resultados para \'" + q + "\'";
+        } else if (totalElements > 1) {
+            message = "Foram encontrados " + totalElements + " resultados para \'" + q + "\'";
         }
 
         model.addAttribute("q", q);
-        model.addAttribute("results", results);
+        model.addAttribute("results", results.getContent());
         model.addAttribute("message", message);
         model.addAttribute("title", "busca: " + q);
 

5e0d944dacf379c26510e1a5ef97e13f50c040c1
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-10 12:01:20 -0300

    Make search methods accept Pageable

--- src/main/java/com/example/uspForum/repository/SubjectRepository.java
+++ src/main/java/com/example/uspForum/repository/SubjectRepository.java
@@ -1,6 +1,7 @@
 package com.example.uspForum.repository;
 
 import com.example.uspForum.model.Subject;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -16,23 +17,23 @@ public interface SubjectRepository extends CrudRepository<Subject, Long>, Paging
             "LOWER(s.normalizedName) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
             "LOWER(s.course.name) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
             "LOWER(s.professor.name) LIKE LOWER(CONCAT('%', :searchText, '%')) ")
-    List<Subject> findSubjectBySearchText(String searchText);
+    List<Subject> findSubjectBySearchText(String searchText, Pageable pageable);
 
     @Query("SELECT s FROM Subject s WHERE " +
             "LOWER(s.abbreviation) LIKE LOWER(CONCAT('%', :abbreviation, '%'))")
-    List<Subject> findSubjectByAbbreviation(String abbreviation);
+    List<Subject> findSubjectByAbbreviation(String abbreviation, Pageable pageable);
 
     @Query("SELECT s FROM Subject s WHERE " +
             "LOWER(s.code) LIKE LOWER(CONCAT('%', :code, '%'))")
-    List<Subject> findSubjectByCode(String code);
+    List<Subject> findSubjectByCode(String code, Pageable pageable);
 
     @Query("SELECT s FROM Subject s WHERE " +
             "LOWER(s.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-    List<Subject> findSubjectByName(String name);
+    List<Subject> findSubjectByName(String name, Pageable pageable);
 
     @Query("SELECT s FROM Subject s WHERE " +
             "LOWER(s.professor.name) LIKE LOWER(CONCAT('%', :teacherName, '%'))")
-    List<Subject> findSubjectByTeacherName(String teacherName);
+    List<Subject> findSubjectByTeacherName(String teacherName, Pageable pageable);
 
     List<Subject> findByCourseNormalizedNameAndCourseCampusAbbreviationAndAbbreviation(
             String course_normalizedName, String course_campus_abbreviation, String abbreviation);

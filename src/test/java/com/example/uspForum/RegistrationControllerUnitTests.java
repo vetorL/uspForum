d00e905650e418f2ed4a07a44d4460cad0bd7ecd
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-11 11:56:58 -0300

    Add testNonUSPEmailRegistering

--- src/test/java/com/example/uspForum/RegistrationControllerUnitTests.java
+++ src/test/java/com/example/uspForum/RegistrationControllerUnitTests.java
@@ -4,6 +4,8 @@ import com.example.uspForum.model.Campus;
 import com.example.uspForum.model.CustomUser;
 import com.example.uspForum.model.RegistrationFormDTO;
 import com.example.uspForum.repository.CustomUserRepository;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,6 +14,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -48,6 +52,25 @@ public class RegistrationControllerUnitTests {
         this.mockMvc.perform(post("/registrar").with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/login"));
+    }
+
+    @Test
+    @DisplayName("Tests case where the user attempts to register an email that does not have the USP domain")
+    void testNonUSPEmailRegistering() throws Exception {
+        String email = "invalid@email.com";
+        String username = "username";
+        String password = "password";
+        String campusAbbr = "TEST";
+
+        this.mockMvc.perform(post("/registrar")
+                    .param("email", email)
+                    .param("username", username)
+                    .param("password", password)
+                    .param("campusAbbr", campusAbbr)
+                    .with(csrf())
+                )
+                .andExpect(view().name("registration"))
+                .andExpect(content().string(containsString("O email deve ser @usp.br")));
     }
 
     @Test

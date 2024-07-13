876332668aada875180fec98598fd6ba120a36be
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-13 00:40:49 -0300

    Add searchType model attribute

--- src/main/java/com/example/uspForum/controller/SearchController.java
+++ src/main/java/com/example/uspForum/controller/SearchController.java
@@ -47,6 +47,7 @@ public class SearchController {
         }
 
         model.addAttribute("q", q);
+        model.addAttribute("searchType", t);
         model.addAttribute("currentPage", p);
         model.addAttribute("results", results);
         model.addAttribute("message", message);

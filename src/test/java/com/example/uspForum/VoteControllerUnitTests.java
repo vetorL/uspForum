ad1a4e500dbd7c20a70724ed8688446f6498550c
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-15 10:52:30 -0300

    Fix testVotingAuthenticated

--- src/test/java/com/example/uspForum/VoteControllerUnitTests.java
+++ src/test/java/com/example/uspForum/VoteControllerUnitTests.java
@@ -1,6 +1,9 @@
 package com.example.uspForum;
 
 import com.example.uspForum.controller.VoteController;
+import com.example.uspForum.model.CustomUser;
+import com.example.uspForum.model.Subject;
+import com.example.uspForum.model.SubjectReview;
 import com.example.uspForum.model.VoteDTO;
 import com.example.uspForum.service.SubjectReviewService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,6 +35,11 @@ public class VoteControllerUnitTests {
     @DisplayName("Tests voting when the user is authenticated.")
     void testVotingAuthenticated() throws Exception {
         VoteDTO voteDTO = new VoteDTO(1, "up");
+
+        // a SubjectReview instance needs to be returned so that subjectReview.getAuthor() may be called upon it
+        // and a CustomUser instance needs to be returned so that setRep and getRep may be called upon it
+        when(subjectReviewService.findById(voteDTO.getSubjectReviewId())).thenReturn(new SubjectReview(new CustomUser(),
+                new Subject(), "title", "content", "recommendation"));
 
         mockMvc.perform(post("/disciplina/votar")
                 .contentType(MediaType.APPLICATION_JSON)

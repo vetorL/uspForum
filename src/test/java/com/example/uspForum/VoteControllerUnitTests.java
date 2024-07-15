728f7495c30a2e64597392d270c85c617ac34d8d
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-15 13:08:24 -0300

    Add testVoteUpWhenAlreadyVotedUp

--- src/test/java/com/example/uspForum/VoteControllerUnitTests.java
+++ src/test/java/com/example/uspForum/VoteControllerUnitTests.java
@@ -155,4 +155,57 @@ public class VoteControllerUnitTests {
         assertEquals(-1, subjectReview.getAuthor().getRep());
     }
 
+    @Test
+    @WithMockUser
+    @DisplayName("Test voting up when the user has already voted up")
+    void testVoteUpWhenAlreadyVotedUp() throws Exception {
+        // Given
+        long subjectReviewId = 1;
+        VoteDTO voteDTO = new VoteDTO(subjectReviewId, "up");
+        CustomUser voter = new CustomUser();
+        SubjectReview subjectReview = new SubjectReview(new CustomUser(),
+                new Subject(), "title", "content", "recommendation");
+
+        // The author already had an upvote from the voter
+        subjectReview.getAuthor().setRep(1);
+
+        // Mocking behavior of findById
+        when(subjectReviewService.findById(voteDTO.getSubjectReviewId())).thenReturn(subjectReview);
+
+        // Mocking behavior of userAlreadyVotedOnReview (makes it return true)
+        when(subjectReviewService.userAlreadyVotedOnReview(any(), any()))
+                .thenReturn(true);
+
+        // Mocking behavior of getUserReviewVoteType (makes it return 1, that corresponds to "up")
+        when(subjectReviewService.getUserReviewVoteType(any(), any())).thenReturn(1);
+
+        // Make POST request
+        mockMvc.perform(post("/disciplina/votar")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(voteDTO))
+                        .with(csrf()))
+                .andExpect(status().isOk());
+
+        // ### VERIFICATIONS ###
+        // The verifications must come after the POST request
+
+        // Verify that findById was called 1 time
+        verify(subjectReviewService, times(1)).findById(anyLong());
+
+        // Verify that userAlreadyVotedOnReview was called 1 time
+        verify(subjectReviewService, times(1)).userAlreadyVotedOnReview(any(), any(SubjectReview.class));
+
+        // Verify that the getUserReviewVoteType method was called 1 time
+        verify(subjectReviewService, times(1)).getUserReviewVoteType(any(), any(SubjectReview.class));
+
+        // Verify that the removeVoteFromReview method was called 1 time
+        verify(subjectReviewService, times(1)).removeVoteFromReview(any(), any(SubjectReview.class));
+
+        // Verify that no other method was called on subjectReviewService after addVoteToReview
+        verifyNoMoreInteractions(subjectReviewService);
+
+        // Check that the subjectReview's author now has a rep of 0
+        assertEquals(0, subjectReview.getAuthor().getRep());
+    }
+
 }

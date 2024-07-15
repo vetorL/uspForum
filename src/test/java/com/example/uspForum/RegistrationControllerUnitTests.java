6ea239e75835fc71b2ad1bea32129530a01ba17f
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-15 11:29:29 -0300

    Remove @AutoConfigureMockMvc annotation
    @WebMvcTest already includes it, so it is redundant here

--- src/test/java/com/example/uspForum/RegistrationControllerUnitTests.java
+++ src/test/java/com/example/uspForum/RegistrationControllerUnitTests.java
@@ -6,7 +6,6 @@ import com.example.uspForum.service.CampusService;
 import com.example.uspForum.service.CustomUserService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -22,7 +21,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(RegistrationController.class)
 @Import(SecurityConfig.class)
-@AutoConfigureMockMvc
 public class RegistrationControllerUnitTests {
 
     @MockBean
--- src/test/java/com/example/uspForum/SearchControllerUnitTests.java
+++ src/test/java/com/example/uspForum/SearchControllerUnitTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -22,7 +21,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(SearchController.class)
 @Import(SecurityConfig.class)
-@AutoConfigureMockMvc
 public class SearchControllerUnitTests {
 
     @Autowired

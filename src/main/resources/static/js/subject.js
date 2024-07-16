7eaf633081cd6b010f836f615e1c7973d3a8c86b
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-16 11:03:39 -0300

    Update character-counter as user writes

--- src/main/resources/static/js/subject.js
+++ src/main/resources/static/js/subject.js
@@ -1,4 +1,4 @@
-// Calling the API
+// Calling the voting API
 
 let buttonForms = document.getElementsByClassName("vote-button-form");
 
@@ -29,7 +29,7 @@ Array.from(buttonForms).forEach(elem => {elem.addEventListener(
     }
 )})
 
-// Handling of the visual response
+// Handling of the visual response of voting
 
 let voteUpButtons = document.getElementsByClassName("vote-up");
 let voteDownButtons = document.getElementsByClassName("vote-down");
@@ -82,4 +82,37 @@ function handleClickDown(e) {
         e.currentTarget.classList.add("vote-pressed-down");
         countElem.innerText = String(Number(countElem.innerText) - 1);
     }
+}
+
+// Update .subject-review-form__character-counter as user writes
+document.getElementById("subject-review-form__content-input")
+    .addEventListener("input", updateCharacterCounter);
+
+document.getElementById("subject-review-form__title-input")
+    .addEventListener("input", updateCharacterCounter);
+
+function updateCharacterCounter(event) {
+
+    const current_number_of_characters = event.currentTarget.value.length;
+
+    const span_character_counter = event.currentTarget.nextElementSibling;
+
+    let max_size = span_character_counter.innerText.split("/")[1];
+
+    span_character_counter.innerText = current_number_of_characters + "/" + max_size;
+
+    if(current_number_of_characters > parseInt(max_size)) {
+
+        // The user has written more characters than allowed, give a visual warning
+        span_character_counter.style.color = "red";
+        event.currentTarget.style.color = "red";
+
+    } else {
+
+        // The user is within the character limit
+        span_character_counter.style.color = "gray";
+        event.currentTarget.style.color = "black";
+
+    }
+
 }

678a2f0f39947a5a4828e18b7d25b32b78e7b3e8
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-16 22:01:43 -0300

    Adapt script to .options-dropdown__background

--- src/main/resources/static/js/subject-review__options-dropdown.js
+++ src/main/resources/static/js/subject-review__options-dropdown.js
@@ -1,20 +1,29 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropdown(e) {
-    console.log(e.nextElementSibling);
     e.nextElementSibling.classList.toggle("show");
+
+    // This invisible background is for capturing clicks outside the dropdown
+    const optionsDropdownBackground = e.nextElementSibling.nextElementSibling;
+    optionsDropdownBackground.style.display = "block";
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.options-dropdown__button')) {
-        var dropdowns = document.getElementsByClassName("subject-review__options-dropdown");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+    console.log(event.target);
+    if (event.target.classList.contains("options-dropdown__background")) {
+        let dropdowns = document.getElementsByClassName("subject-review__options");
+        Array.from(dropdowns).forEach(element => {
+            if (element.classList.contains('show')) {
+                element.classList.remove('show');
             }
-        }
+        });
+
+        let dropdownBackgrounds =
+            document.getElementsByClassName("options-dropdown__background");
+
+        Array.from(dropdownBackgrounds).forEach(element => {
+            element.style.display = "none";
+        });
     }
 }

33200853efafd090b3c3ae0ae854eb1ee15d8ad5
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-17 12:11:54 -0300

    Create SubjectReviewController

---
+++ src/main/java/com/example/uspForum/controller/SubjectReviewController.java
@@ -0,0 +1,32 @@
+package com.example.uspForum.controller;
+
+import com.example.uspForum.model.CustomUser;
+import com.example.uspForum.model.SubjectReview;
+import com.example.uspForum.service.SubjectReviewService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/reviews/{id}")
+public class SubjectReviewController {
+
+    private final SubjectReviewService subjectReviewService;
+
+    public SubjectReviewController(SubjectReviewService subjectReviewService) {
+        this.subjectReviewService = subjectReviewService;
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable("id") long id,
+                       @AuthenticationPrincipal CustomUser user) {
+
+        SubjectReview subjectReview = subjectReviewService.findById(id);
+
+        if (subjectReview != null) {
+            subjectReviewService.delete(subjectReview);
+        }
+        
+    }
+
+}

e40d4a7dbe0e345b63fdad9fd47de1224d42cb66
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-18 00:34:44 -0300

    Annotate with @RequestMapping

--- src/main/java/com/example/uspForum/controller/SubjectReviewController.java
+++ src/main/java/com/example/uspForum/controller/SubjectReviewController.java
@@ -6,9 +6,11 @@ import com.example.uspForum.service.SubjectReviewService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/reviews/{id}")
+@RestController
+@RequestMapping("/api/v1/reviews/{id}")
 public class SubjectReviewController {
 
     private final SubjectReviewService subjectReviewService;

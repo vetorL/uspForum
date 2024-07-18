a633718b698f347e4b3f1f977f42f3313c9f674a
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-17 23:37:04 -0300

    Add initial modal
    Still lots to be worked on, the current modal only works properly if there is no
    more than 1 review on the page

--- src/main/resources/static/js/subject-review__options-dropdown.js
+++ src/main/resources/static/js/subject-review__options-dropdown.js
@@ -25,4 +25,28 @@ window.onclick = function(event) {
             element.style.display = "none";
         });
     }
+
+    // When the user clicks anywhere outside of the modal, close it
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }

17170379e888409d337be2340495b2811b56c7dd
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-18 11:28:16 -0300

    Create NotFoundException

---
+++ src/main/java/com/example/uspForum/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.uspForum.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}

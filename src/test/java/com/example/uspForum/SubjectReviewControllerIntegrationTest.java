82e759f9fa5de6aa9bc3e0f912685296700397cd
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-20 00:07:59 -0300

    Add deleteSuccessfulWhenUserIsAuthor test

--- src/test/java/com/example/uspForum/SubjectReviewControllerIntegrationTest.java
+++ src/test/java/com/example/uspForum/SubjectReviewControllerIntegrationTest.java
@@ -1,13 +1,16 @@
 package com.example.uspForum;
 
-import com.example.uspForum.config.SecurityConfig;
 import com.example.uspForum.controller.SubjectReviewController;
+import com.example.uspForum.model.*;
+import com.example.uspForum.repository.*;
 import jakarta.servlet.ServletContext;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockServletContext;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -15,7 +18,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {UspForumApplication.class})
@@ -24,6 +31,24 @@ public class SubjectReviewControllerIntegrationTest {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private SubjectReviewRepository subjectReviewRepository;
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Autowired
+    private CustomUserRepository customUserRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private CampusRepository campusRepository;
+
+    @Autowired
+    private ProfessorRepository professorRepository;
 
     private MockMvc mockMvc;
 
@@ -39,6 +64,32 @@ public class SubjectReviewControllerIntegrationTest {
         assertNotNull(servletContext);
         assertInstanceOf(MockServletContext.class, servletContext);
         assertNotNull(webApplicationContext.getBean(SubjectReviewController.class));
+    }
+
+    @Test
+    @WithMockUser(username = "test")
+    @DisplayName("Test that the review gets deleted when user who made the DELETE request is the author.")
+    void deleteSuccessfulWhenUserIsAuthor() throws Exception {
+        Campus each = campusRepository.save(new Campus("Escola de Artes, Ciências e Humanidades",
+                "EACH"));
+
+        Course si = courseRepository.save(new Course("Sistemas de Informação",
+                "sistemas-de-informacao", each));
+
+        CustomUser usr = customUserRepository.save(new CustomUser("test@test.com", "test",
+                "password", each));
+
+        Professor violeta = professorRepository.save(new Professor("Violeta Sun", "violeta-sun",
+                "violeta@usp.br", each));
+
+        Subject iaecVioleta = subjectRepository.save(new Subject("Introdução à Administração e Economia para Computação",
+                "IAEC", "ACH2063", si, violeta));
+
+        SubjectReview subjectReview = subjectReviewRepository.save(new SubjectReview(usr, iaecVioleta, "Foi bom",
+                "Nao teve prova, mas teve varios trabalhinhos", "Recomendo"));
+
+        mockMvc.perform(delete("/api/v1/reviews/" + subjectReview.getId()).with(csrf()))
+                .andExpect(status().isNoContent());
     }
 
 }

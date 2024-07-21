df45d556b7a554d222e12ad5e049bf67ebc20222
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-21 12:16:44 -0300

    Create SubjectReviewResponse

--- src/main/java/com/example/uspForum/model/SubjectReviewResponse.java
+++ src/main/java/com/example/uspForum/model/SubjectReviewResponse.java
@@ -1,2 +1,26 @@
-package com.example.uspForum.model;public class SubjectReviewResponse {
+package com.example.uspForum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubjectReviewResponse {
+
+    private long id;
+    private String title;
+    private String content;
+    private String recommendation;
+
+    public static SubjectReviewResponse fromSubjectReview(SubjectReview subjectReview) {
+        return new SubjectReviewResponse(
+                subjectReview.getId(),
+                subjectReview.getTitle(),
+                subjectReview.getContent(),
+                subjectReview.getRecommendation()
+        );
+    }
+
 }

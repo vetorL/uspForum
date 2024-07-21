b12ef9d7c4c660932deeebbe5e8026b75f8dcb4c
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-21 11:23:50 -0300

    Implement update endpoint

--- src/main/java/com/example/uspForum/controller/SubjectReviewController.java
+++ src/main/java/com/example/uspForum/controller/SubjectReviewController.java
@@ -1,13 +1,14 @@
 package com.example.uspForum.controller;
 
+import com.example.uspForum.model.CustomUser;
+import com.example.uspForum.model.Subject;
 import com.example.uspForum.model.SubjectReview;
+import com.example.uspForum.model.SubjectReviewDTO;
 import com.example.uspForum.service.SubjectReviewService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/reviews/{id}")
@@ -17,6 +18,19 @@ public class SubjectReviewController {
 
     public SubjectReviewController(SubjectReviewService subjectReviewService) {
         this.subjectReviewService = subjectReviewService;
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody SubjectReviewDTO subjectReviewDTO,
+                                    @AuthenticationPrincipal CustomUser author) {
+
+        SubjectReview subjectReview = subjectReviewService.findById(id);
+        Subject subject = subjectReview.getSubject();
+
+        subjectReviewService.delete(subjectReview);
+        subjectReviewService.create(subjectReviewDTO.toSubjectReview(author, subject));
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping

3ac547724095a7275351cc9602874a765a0e667e
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-21 17:27:49 -0300

    Add ObjectMapper bean

--- src/main/java/com/example/uspForum/UspForumApplication.java
+++ src/main/java/com/example/uspForum/UspForumApplication.java
@@ -1,13 +1,20 @@
 package com.example.uspForum;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UspForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UspForumApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
 	}
 
 }

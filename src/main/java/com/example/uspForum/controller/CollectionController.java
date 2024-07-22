83efd47f8b9eeabecb7675631705fad12fd19a96
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-22 19:33:45 -0300

    Create read enpoint in CollectionController

---
+++ src/main/java/com/example/uspForum/controller/CollectionController.java
@@ -0,0 +1,38 @@
+package com.example.uspForum.controller;
+
+import com.example.uspForum.model.Subject;
+import com.example.uspForum.service.SubjectService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/arquivo/{campus}/{course}/{subject}/{professor}/acervo")
+public class CollectionController {
+
+    private final SubjectService subjectService;
+
+    private CollectionController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @GetMapping
+    public String read(@PathVariable("campus") String campusAbbreviation,
+                       @PathVariable("course") String courseNormalizedName,
+                       @PathVariable("subject") String subjectAbbreviation,
+                       @PathVariable("professor") String professorNormalizedName,
+                       Model model) {
+
+        Subject subject = subjectService.findByCourseAndCampusAndSubjectAndProfessor(
+                courseNormalizedName, campusAbbreviation, subjectAbbreviation, professorNormalizedName);
+
+        model.addAttribute("subject", subject);
+        model.addAttribute("title", "Acervo " + subjectAbbreviation + " - " +
+                subject.getProfessor().getName());
+
+        return "collection";
+    }
+
+}

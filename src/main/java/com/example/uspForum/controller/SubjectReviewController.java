568456d5dc60b3d26cc553582f6cf21a0fca7223
vetorL <145482274+vetorL@users.noreply.github.com>
2024-07-24 12:58:38 -0300

    Implement create REST endpoint for subject reviews

--- src/main/java/com/example/uspForum/controller/SubjectReviewController.java
+++ src/main/java/com/example/uspForum/controller/SubjectReviewController.java
@@ -1,12 +1,11 @@
 package com.example.uspForum.controller;
 
-import com.example.uspForum.model.CustomUser;
-import com.example.uspForum.model.SubjectReview;
-import com.example.uspForum.model.SubjectReviewDTO;
-import com.example.uspForum.model.SubjectReviewResponse;
+import com.example.uspForum.model.*;
 import com.example.uspForum.service.SubjectReviewService;
+import com.example.uspForum.service.SubjectService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +15,24 @@ import org.springframework.web.bind.annotation.*;
 public class SubjectReviewController {
 
     private final SubjectReviewService subjectReviewService;
+    private final SubjectService subjectService;
 
-    public SubjectReviewController(SubjectReviewService subjectReviewService) {
+    public SubjectReviewController(SubjectReviewService subjectReviewService, SubjectService subjectService) {
         this.subjectReviewService = subjectReviewService;
+        this.subjectService = subjectService;
+    }
+
+    @PostMapping(value = "/subject/{subjectId}/reviews", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> create(@PathVariable("subjectId") long subjectId,
+                                    @AuthenticationPrincipal CustomUser author,
+                                    @Valid @RequestBody SubjectReviewDTO subjectReviewDTO) {
+
+        Subject associatedSubject = subjectService.findById(subjectId);
+        SubjectReview subjectReview = subjectReviewDTO.toSubjectReview(author, associatedSubject);
+
+        subjectService.postSubjectReview(subjectReview);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/reviews/{id}")

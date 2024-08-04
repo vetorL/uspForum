b659bf1a7f5d6d86337b8f4ad48c76ff36b98e17
vetorL <145482274+vetorL@users.noreply.github.com>
2024-08-04 14:36:34 -0300

    Add findAllBySender in ContactRepository

--- src/main/java/com/example/uspForum/contact/ContactRepository.java
+++ src/main/java/com/example/uspForum/contact/ContactRepository.java
@@ -1,6 +1,12 @@
 package com.example.uspForum.contact;
 
+import com.example.uspForum.customUser.CustomUser;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ContactRepository extends CrudRepository<Contact, Long> {
+
+    List<Contact> findAllBySender(CustomUser sender);
+
 }

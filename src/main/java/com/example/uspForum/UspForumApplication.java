d3cd024a47aff31cbe31988fea6ffc5f44fecbdb
vetorL <145482274+vetorL@users.noreply.github.com>
2024-08-04 11:51:31 -0300

    Define ModelMapper as Bean

--- src/main/java/com/example/uspForum/UspForumApplication.java
+++ src/main/java/com/example/uspForum/UspForumApplication.java
@@ -1,5 +1,6 @@
 package com.example.uspForum;
 
+import com.example.uspForum.util.ModelMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,11 @@ public class UspForumApplication {
 	@Bean
 	public ObjectMapper objectMapper() {
 		return new ObjectMapper();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }

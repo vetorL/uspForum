8f5d21aa56f5e9b5a5b939af367d4dabe7a7cd0f
vetorL <145482274+vetorL@users.noreply.github.com>
2024-08-05 10:39:41 -0300

    Implement isOlderThanOneDay method in DateHandler
    This will be initially used for checking if the user can contact again

--- src/main/java/com/example/uspForum/util/DateHandler.java
+++ src/main/java/com/example/uspForum/util/DateHandler.java
@@ -2,6 +2,23 @@ package com.example.uspForum.util;
 
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
 @Component
 public class DateHandler {
+
+    public boolean isOlderThanOneDay(Date dateToCheck) {
+        // Convert Date to Instant
+        Instant instantToCheck = dateToCheck.toInstant();
+        Instant now = Instant.now();
+
+        // Calculate the duration between now and the date to check
+        Duration duration = Duration.between(instantToCheck, now);
+
+        // Check if the duration is greater than one day
+        return duration.toDays() >= 1;
+    }
+
 }
---
+++ src/test/java/com/example/uspForum/util/DateHandlerTests.java
@@ -0,0 +1,52 @@
+package com.example.uspForum.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+public class DateHandlerTests {
+
+    @Test
+    @DisplayName("isOlderThanOneDay returns false correctly")
+    void isOlderThanOneDayReturnsFalseCorrectly() {
+        DateHandler dateHandler = new DateHandler();
+
+        Date date = new Date();
+        // 86400000 Milliseconds represents exactly one day, so 86399999 is one millisecond away from exactly one day
+        date.setTime(date.getTime() - 86399999);
+
+        assertFalse(dateHandler.isOlderThanOneDay(date));
+    }
+
+    @Test
+    @DisplayName("isOlderThanOneDay returns true correctly when exactly one day has passed")
+    void isOlderThanOneDayReturnsTrueCorrectlyWhenExactlyOneDayHasPassed() {
+        DateHandler dateHandler = new DateHandler();
+
+        Date date = new Date();
+        // 86400000 Milliseconds represents exactly one day
+        date.setTime(date.getTime() - 86400000);
+
+        assertTrue(dateHandler.isOlderThanOneDay(date));
+    }
+
+    @Test
+    @DisplayName("isOlderThanOneDay returns true correctly when more than one day has passed")
+    void isOlderThanOneDayReturnsTrueCorrectlyWhenMoreThanOneDayHasPassed() {
+        DateHandler dateHandler = new DateHandler();
+
+        Date date = new Date();
+        // 86400000 Milliseconds represents exactly one day
+        date.setTime(date.getTime() - 86400001);
+
+        assertTrue(dateHandler.isOlderThanOneDay(date));
+    }
+
+}

3bca15d6bb5d33a6cd10ef0d8a98c9c33dc4e77d
vetorL <145482274+vetorL@users.noreply.github.com>
2024-08-06 11:32:46 -0300

    Create ReviewReport model
    This class is going to be used to model user reports to subject reviews

---
+++ src/main/java/com/example/uspForum/report/ReviewReport.java
@@ -0,0 +1,24 @@
+package com.example.uspForum.report;
+
+import com.example.uspForum.subjectReview.SubjectReview;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
+public class ReviewReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private final String reason;
+
+    @ManyToOne
+    @JoinColumn(name = "subject_review_id")
+    private final SubjectReview subjectReview;
+}

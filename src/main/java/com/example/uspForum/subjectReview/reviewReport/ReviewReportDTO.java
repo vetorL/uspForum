39c5c2761a99ab7a0ef8f3f4ec2ef1bea912fc86
vetorL <145482274+vetorL@users.noreply.github.com>
2024-08-06 13:22:27 -0300

    Create ReviewReportDTO

---
+++ src/main/java/com/example/uspForum/subjectReview/reviewReport/ReviewReportDTO.java
@@ -0,0 +1,14 @@
+package com.example.uspForum.subjectReview.reviewReport;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewReportDTO {
+
+    private String reason;
+
+}

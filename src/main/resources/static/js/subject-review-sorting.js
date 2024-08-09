37afb4e0e1bb6711209221de7d9af954586cefdf
vetorL <145482274+vetorL@users.noreply.github.com>
2024-08-09 11:10:46 -0300

    Stop keeping scroll position after refresh
    This was causing the same scroll pos to be kept for all subject pages

--- src/main/resources/static/js/subject-review-sorting.js
+++ src/main/resources/static/js/subject-review-sorting.js
@@ -1,15 +1,3 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    var scrollpos = sessionStorage.getItem('scrollpos');
-    if (scrollpos) {
-        window.scrollTo(0, scrollpos);
-        sessionStorage.removeItem('scrollpos');
-    }
-});
-
-window.addEventListener("beforeunload", function (e) {
-    sessionStorage.setItem('scrollpos', window.scrollY);
-});
-
 // Handle sorting
 document.getElementById("subject-reviews-filter").addEventListener("input", sort);
 
